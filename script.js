be9341c9ca62e1e5d84d0ba3011e8c41ac9b6e64
khadija <khadijaceurrouj57@gmail.com>
2025-09-17 13:47:57 +0100

    fich

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-function addition(a, b) {
-  return a + b;
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// let somme = addition(8, 5);
+// console.log("La somme est : " + somme);
+
+
+
+//   chaptre 9 
+//  let age = 20;
+
+// if (age < 18) {
+//   console.log("Vous êtes mineur.");
+// } else if (age === 18) {
+//   console.log("Vous venez d’atteindre la majorité !");
+// } else {
+//   console.log("Vous êtes majeur.");
+// }
+//chaptre 10
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) continue; // sauter le 5
+  console.log("Nombre : " + i);
 }
 
-let somme = addition(8, 5);
-console.log("La somme est : " + somme);
-
-
-
-  //chaptre 9 
- let age = 20;
-
-if (age < 18) {
-  console.log("Vous êtes mineur.");
-} else if (age === 18) {
-  console.log("Vous venez d’atteindre la majorité !");
-} else {
-  console.log("Vous êtes majeur.");
-}
